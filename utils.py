e665c98cbe8b71a12d48791ab6fccf6bafde2a25
tristan <tristan.hascoet@gmail.com>
2019-01-16 01:02:55 +0900

    Add train and valid helper functions to utils

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
 import numpy as np
+import torch
+import torch.nn.functional as F
+
+topy = lambda x: torch.from_numpy(x).cuda(device)
 
 def pad_size(ks, mode):
     assert mode in ['valid', 'same', 'full']
@@ -43,3 +47,43 @@ def distrib_mod(model):
         res[mod]=len(layers)
     return res
 
+def train(ds):
+    model.train()
+    x,y = ds.sample()
+    x = topy(x)
+    y = topy(y)
+
+    w_pos = y.mean(2, keepdim=True).mean(3, keepdim=True).mean(4, keepdim=True)
+    w_neg = 1 - w_pos
+    msk_pos = (y == 1).float()
+    msk_neg = (y == 0).float()
+    msk = n_msk = msk_pos * w_neg + msk_neg * w_pos
+
+    opt.zero_grad()
+    out = model(x)
+    loss = F.binary_cross_entropy_with_logits(out, y, msk)
+    loss.backward()
+    opt.step()
+    return loss.item()
+    
+def valid(ds, nrun=20):
+    model.eval()
+    with torch.no_grad():
+        accs = 0
+        for i in range(nrun):
+            x,y = ds.sample()
+            x = topy(x)
+            y = topy(y)
+
+            w_pos = y.mean(2, keepdim=True).mean(3, keepdim=True).mean(4, keepdim=True)
+            w_neg = 1 - w_pos
+            msk_pos = (y == 1).float()
+            msk_neg = (y == 0).float()
+            msk = n_msk = msk_pos * w_neg + msk_neg * w_pos
+            
+            out = model(x)
+            accs += F.binary_cross_entropy_with_logits(out, y, msk).item()
+    return accs / nrun
+    
+def monitor(name, y, x):
+    writer.add_scalar(name, y, x)
